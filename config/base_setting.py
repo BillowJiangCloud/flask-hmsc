54f2113afe9bf94bb45f653c554fcacdee633857
TaoTao <Tao@mail.com>
2020-04-28 01:31:25 +0800

    数据库建立、创建账号、商品管理、今日数据等

--- config/base_setting.py
+++ config/base_setting.py
@@ -1,3 +1,15 @@
 SERVER_PORT = 5000
 SQLALCHEMY_DATABASE_URI = 'mysql://root:19970218@127.0.0.1/hmsc?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Cookie
+AUTH_COOKIE_NAME = "hmsc_1901C"
+
+# 设置拦截器忽略规则
+IGNORE_URLS = [
+    "^/user/login"
+]
+IGNORE_CHECK_LOGIN_URLS = [
+    "^/static",
+    "^/favicon.ico"
+]
--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,6 +1,11 @@
-from flask import Blueprint, render_template, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, redirect, g
+
+from application import app, db
 from common.models.User import User
-from common.libs.user.userService import UserService
+from common.libs.user.UserService import UserService
+from common.libs.UrlManager import UrlManager
+from common.libs.Helper import ops_render
+
 import json
 
 router_user = Blueprint('user_page', __name__)
@@ -9,8 +14,11 @@ router_user = Blueprint('user_page', __name__)
 @router_user.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template("user/login.html")
+        if g.current_user:
+            return redirect(UrlManager.buildUrl("/"))
+        return ops_render("user/login.html")
 
+    # POST请求
     resp = {'code': 200, 'msg': '登录成功', 'data': {}}
     req = request.values
     login_name = req['login_name'] if 'login_name' in req else ''
@@ -28,13 +36,13 @@ def login():
     user_info = User.query.filter_by(login_name=login_name).first()
     if not user_info:
         resp['code'] = -1
-        resp['msg'] = '用户不存在'
+        resp['msg'] = "用户不存在"
         return jsonify(resp)
     # 判断密码
     if user_info.login_pwd != UserService.generatePwd(login_pwd,
                                                       user_info.login_salt):
         resp['code'] = -1
-        resp['msg'] = '密码输入错误'
+        resp['msg'] = "密码输入错误"
         return jsonify(resp)
 
     # 判断用户状态
@@ -42,29 +50,93 @@ def login():
         resp['code'] = -1
         resp['msg'] = "用户已经被禁用，请联系管理员处理"
         return jsonify(resp)
-    print(user_info)
-    response = make_response(json.dumps({'code': 200, 'msg': '登陆成功~~~'}))
+
+    response = make_response(json.dumps({'code': 200, 'msg': '登录成功~~~'}))
     # Cookie中存入的信息是user_info.uid,user_info
     response.set_cookie(
-        "hmsc_1901C", "%s@%s" % (
-            UserService.generateAuthCode(user_info),
-            user_info.uid,
-        ), 60 * 60 * 24 * 15)
+        app.config['AUTH_COOKIE_NAME'],
+        "%s@%s" % (UserService.generateAuthCode(user_info), user_info.uid),
+        60 * 60 * 24 * 15)
     return response
-
-    # return jsonify(resp)
 
 
 @router_user.route("/logout")
 def logout():
-    return "登出"
+    response = make_response(redirect(UrlManager.buildUrl("/user/login")))
+    response.delete_cookie(app.config['AUTH_COOKIE_NAME'])
+    return response
 
 
-@router_user.route("/edit")
+@router_user.route("/edit", methods=['GET', 'POST'])
 def edit():
-    return "编辑"
+    if request.method == "GET":
+        return ops_render("user/edit.html")
+    # POST请求
+    resp = {'code': 200, 'msg': '编辑成功', 'data': {}}
+
+    req = request.values
+    nickname = req['nickname'] if 'nickname' in req else ''
+    email = req['email'] if 'email' in req else ''
+    if nickname is None or len(nickname) < 1:
+        resp['code'] = -1
+        resp['msg'] = "请输入规范的nickname"
+        return jsonify(resp)
+    if email is None or len(email) < 1:
+        resp['code'] = -1
+        resp['msg'] = "请输入规范的email"
+        return jsonify(resp)
+
+    # 别忘了g
+    user_info = g.current_user
+    user_info.nickname = nickname
+    user_info.email = email
+
+    db.session.add(user_info)
+    db.session.commit()
+    return jsonify(resp)
 
 
-@router_user.route("/reset-pwd")
+@router_user.route("/reset-pwd", methods=['GET', 'POST'])
 def resetPwd():
-    return "重置密码"
+    if request.method == "GET":
+        return ops_render("user/reset_pwd.html")
+    # POST请求
+    resp = {'code': 200, 'msg': '重置密码成功', 'data': {}}
+
+    req = request.values
+    old_password = req['old_password'] if 'old_password' in req else ''
+    new_password = req['new_password'] if 'new_password' in req else ''
+
+    if old_password is None or len(old_password) < 6:
+        resp['code'] = -1
+        resp['msg'] = "请输入符合规范的旧密码"
+        return jsonify(resp)
+    if new_password is None or len(new_password) < 6:
+        resp['code'] = -1
+        resp['msg'] = "请输入符合规范的新密码"
+        return jsonify(resp)
+
+    if old_password == new_password:
+        resp['code'] = -1
+        resp['msg'] = "新密码和旧密码不能相同"
+        return jsonify(resp)
+
+    user_info = g.current_user
+    # 演示账号的保护
+    # if user_info.uid == 1:
+    #     pass
+
+    user_info.login_pwd = UserService.generatePwd(new_password,
+                                                  user_info.login_salt)
+
+    db.session.add(user_info)
+    db.session.commit()
+
+    # 修改cookie中的旧用户信息
+    response = make_response(json.dumps(resp))
+    # Cookie中存入的信息是user_info.uid,user_info
+    response.set_cookie(
+        app.config['AUTH_COOKIE_NAME'],
+        "%s@%s" % (UserService.generateAuthCode(user_info), user_info.uid),
+        60 * 60 * 24 * 15)
+    return response
