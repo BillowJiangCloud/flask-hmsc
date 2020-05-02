9e6f0bc3eeedfaeca7f0455080dbf5b0057c21e6
TaoTao <Tao@mail.com>
2020-05-02 22:53:04 +0800

    编辑器，上传文件等功能测试通过，v1

--- README.md
+++ README.md
@@ -10,7 +10,32 @@ create database hmsc;
 
 ###### 二、创建表格/插入数据
 
-**1.创建stat_daily_site表**
+**1.用户表**
+
+```mysql
+# 结构
+create table `user` (
+ `uid` bigint(20) not null auto_increment comment '用户id',
+ `nickname` varchar(100) not null default '' comment '用户昵称',
+ `mobile` varchar(20) not null default '' comment '手机号码',
+ `email` varchar(100) not null default '' comment '用户邮箱',
+ `sex` tinyint(1) not null default '0' comment '1:男 2:女 0:没有填写',
+ `avatar` varchar(64) not null default '' comment '头像',
+ `login_name` varchar(20) not null default '' comment '登录用户名',
+ `login_pwd` varchar(32) not null default '' comment '登录密码',
+ `login_salt` varchar(32) not null default '' comment '登录密码的随机密钥',
+ `status` tinyint(1) not null default '1' comment '1:有效 0:无效',
+ `updated_time` timestamp not null default current_timestamp comment '最后一次更新时间',
+ `created_time` timestamp not null default current_timestamp comment '创建时间',
+ primary key (`uid`),
+ unique key `login_name` (`login_name`)
+)ENGINE=InnoDB default charset=utf8 comment='用户表（管理员）';
+
+# 插入数据(密码123456)
+insert into `user` (`uid`,`nickname`,`mobile`,`email`,`sex`,`avatar`,`login_name`,`login_pwd`,`login_salt`,`status`,`updated_time`,`created_time`) values (1,'BruceNick','12345678945','Bruce@aliyun.com',1,'','admin','816440c40b7a9d55ff9eb7b20760862c','cF3JfH5FJfQ8B2Ba',1,'2020-04-23 11:30:30','2020-04-23 11:10:30');
+```
+
+**2.创建stat_daily_site表**
 
 ```mysql
 # 切换到hmsc库
@@ -21,7 +46,7 @@ drop table if exists stat_daily_site;
 create table stat_daily_site ( id int(11) unsigned not null auto_increment, date date not null comment '日期', total_pay_money decimal(10,2) not null default '0.00' comment '当日收入总额', total_member_count int(11) not null comment '会员总数', total_new_member_count int(11) not null comment '当日新增会员数', total_order_count int(11) not null comment '当日订单数', total_shared_count int(11) not null comment '分享总数', updated_time timestamp not null default current_timestamp comment '最近更新时间', created_time timestamp not null default current_timestamp comment '插入时间', primary key (id), key idx_data (date) )engine=InnoDB default charset=utf8 comment='全站日统计';
 ```
 
-**2.member会员账号**
+**3.member会员账号**
 
 （1.）会员账号/数据库
 
@@ -91,5 +116,33 @@ flask-sqlacodegen 'mysql://root:password@127.0.0.1/hmsc' --tables member_comment
 
 ```mysql
 insert into `member_comments` (`id`,`member_id`,`goods_id`,`pay_order_id`,`score`,`content`,`created_time`) values (1,1,'1',1,'10','好，good','2020-04-29 11:10:30');
+```
+
+**4.商品表**
+
+```mysql
+drop table if exists `goods`;
+
+create table `goods`(
+    `id` int(11) unsigned not null auto_increment,
+    `cat_id` int(11) not null default '0' comment '分类id',
+    `name` varchar(100) not null default '' comment '商品名称',
+    `price` decimal(10,2) not null default '0.00' comment '商品价格',
+    `main_image` varchar(100) not null default '' comment '商品主图',
+    `summary` varchar(10000) not null default '' comment '商品描述',
+    `stock` int(11) not null default '0' comment '库存数',
+    `tags` varchar(200) not null default '' comment 'tag 标签， 用‘,’连接',
+    `status` tinyint(1) not null default '1' comment '1：有效，0：无效',
+    `month_count` int(11) not null default '0' comment '月销量',
+    `total_count` int(11) not null default '0' comment '总销量',
+    `view_count` int(11) not null default '0' comment '总浏览次数',
+    `comment_count` int(11) not null default '0' comment '总评论数',
+    `updated_time` timestamp not null default current_timestamp comment '最后一次更新时间',
+    `created_time` timestamp not null default current_timestamp comment '创建时间',
+    primary key(`id`)
+)ENGINE=InnoDB default charset=utf8 comment='商品表';
+
+
+flask-sqlacodegen 'mysql://root:password@127.0.0.1/hmsc_db' --tables goods --outfile "common/models/goods/Goods.py" --flask
 ```
 
--- web/controllers/member/Member.py
+++ web/controllers/member/Member.py
@@ -1,69 +1,71 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint,request,redirect,jsonify
-from common.libs.Helper import ops_render,iPagination,getCurrentDate
+from flask import Blueprint, request, redirect, jsonify
+from common.libs.Helper import ops_render, iPagination, getCurrentDate
 from common.libs.UrlManager import UrlManager
 from common.models.member.Member import Member
 from common.models.member.MemberComments import MemberComment
-from application import app,db
-router_member = Blueprint( 'member_page',__name__ )
+from application import app, db
+router_member = Blueprint('member_page', __name__)
 
-@router_member.route( "/index" )
+
+@router_member.route("/index")
 def index():
     resp_data = {}
     req = request.values
-    page  = int( req['p'] ) if ( 'p' in req and req['p'] ) else 1
+    page = int(req['p']) if ('p' in req and req['p']) else 1
     query = Member.query
 
     if 'mix_kw' in req:
-        query = query.filter( Member.nickname.ilike( "%{0}%".format( req['mix_kw'] ) ) )
+        query = query.filter(
+            Member.nickname.ilike("%{0}%".format(req['mix_kw'])))
 
-    if 'status' in req and int( req['status'] ) > -1 :
-        query = query.filter( Member.status == int( req['status'] ) )
+    if 'status' in req and int(req['status']) > -1:
+        query = query.filter(Member.status == int(req['status']))
 
     page_params = {
-        'total':query.count(),
+        'total': query.count(),
         'page_size': app.config['PAGE_SIZE'],
-        'page':page,
-        'url': request.full_path.replace("&p={}".format(page),"")
+        'page': page,
+        'url': request.full_path.replace("&p={}".format(page), "")
     }
 
-    pages = iPagination( page_params )
-    offset = ( page - 1 ) * app.config['PAGE_SIZE']
-    list = query.order_by( Member.id.desc() ).offset( offset ).limit( app.config['PAGE_SIZE'] ).all()
+    pages = iPagination(page_params)
+    offset = (page - 1) * app.config['PAGE_SIZE']
+    list = query.order_by(Member.id.desc()).offset(offset).limit(
+        app.config['PAGE_SIZE']).all()
 
     resp_data['list'] = list
     resp_data['pages'] = pages
     resp_data['search_con'] = req
-    resp_data['status'] = {
-        "1":"正常",
-        "0":"已删除"
-    }
+    resp_data['status'] = {"1": "正常", "0": "已删除"}
     resp_data['current'] = 'index'
-    return ops_render( "member/index.html",resp_data )
+    return ops_render("member/index.html", resp_data)
 
-@router_member.route( "/info" )
+
+@router_member.route("/info")
 def info():
     resp_data = {}
     req = request.args
-    id = int( req.get( "id",0 ) )
-    reback_url = UrlManager.buildUrl( "/member/index" )
+    id = int(req.get("id", 0))
+    reback_url = UrlManager.buildUrl("/member/index")
     if id < 1:
-        return redirect( reback_url )
+        return redirect(reback_url)
 
-    info = Member.query.filter_by( id =id ).first()
+    info = Member.query.filter_by(id=id).first()
     if not info:
-        return redirect( reback_url )
+        return redirect(reback_url)
 
     resp_data['info'] = info
     resp_data['current'] = 'index'
-    return ops_render( "member/info.html",resp_data )
+    return ops_render("member/info.html", resp_data)
 
-@router_member.route( "/set",methods = [ "GET","POST" ] )
+
+@router_member.route("/set", methods=["GET", "POST"])
 def set():
     if request.method == "GET":
         resp_data = {}
         req = request.args
-        id = int( req.get( "id",0 ) )
+        id = int(req.get("id", 0))
         reback_url = UrlManager.buildUrl("/member/index")
         if id < 1:
             return redirect(reback_url)
@@ -77,16 +79,16 @@ def set():
 
         resp_data['info'] = info
         resp_data['current'] = 'index'
-        return ops_render( "member/set.html",resp_data )
+        return ops_render("member/set.html", resp_data)
 
-    resp = { 'code':200,'msg':'操作成功~~','data':{} }
+    resp = {'code': 200, 'msg': '操作成功~~', 'data': {}}
     req = request.values
     id = req['id'] if 'id' in req else 0
     nickname = req['nickname'] if 'nickname' in req else ''
-    if nickname is None or len( nickname ) < 1:
+    if nickname is None or len(nickname) < 1:
         resp['code'] = -1
         resp['msg'] = "请输入符合规范的姓名~~"
-        return jsonify( resp )
+        return jsonify(resp)
 
     member_info = Member.query.filter_by(id=id).first()
     if not member_info:
@@ -96,12 +98,12 @@ def set():
 
     member_info.nickname = nickname
     member_info.updated_time = getCurrentDate()
-    db.session.add( member_info )
+    db.session.add(member_info)
     db.session.commit()
-    return jsonify( resp )
+    return jsonify(resp)
 
 
-@router_member.route( "/comment" )
+@router_member.route("/comment")
 def comment():
     resp_data = {}
     req = request.args
@@ -120,23 +122,16 @@ def comment():
 
     comment_list = query.all()
     resp_data['list'] = comment_list
-    member_info = {
-        'avatar':"",
-        'nickname':"Bruce"
-    }
+    member_info = {'avatar': "", 'nickname': "Bruce"}
     for item in comment_list:
         item.member_info = member_info
     resp_data['pages'] = pages
-    return ops_render( "member/comment.html",resp_data )
+    return ops_render("member/comment.html", resp_data)
 
 
-@router_member.route('removeOrRecover',methods=["GET","POST"])
+@router_member.route('removeOrRecover', methods=["GET", "POST"])
 def removeOrRecover():
-    resp = {
-        'code':200,
-        'msg':'操作成功',
-        'data':{}
-    }
+    resp = {'code': 200, 'msg': '操作成功', 'data': {}}
     req = request.values
     id = req['id'] if 'id' in req else 0
     acts = req['acts'] if 'acts' in req else ''
@@ -144,11 +139,11 @@ def removeOrRecover():
         resp['code'] = -1
         resp['msg'] = "未操作操作账号"
         return jsonify(resp)
-    if acts not in ['remove','recover']:
+    if acts not in ['remove', 'recover']:
         resp['code'] = -1
         resp['msg'] = "操作有误"
         return jsonify(resp)
-    
+
     member_info = Member.query.filter_by(id=id).first()
     if not member_info:
         resp['code'] = -1
@@ -158,7 +153,7 @@ def removeOrRecover():
         member_info.status = 0
     elif acts == "recover":
         member_info.status = 1
-        
+
     member_info.updated_time = getCurrentDate()
     db.session.add(member_info)
     db.session.commit()
